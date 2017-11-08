938c7bb8eac9463f6c9700df81637b8d3d2206b3
Qingshan Luo <shanshan.lqs@gmail.com>
2017-11-08 16:52:10 +0800

    [Container] Add a wrapper container.

---
+++ Wrapper.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Container;
+
+class Wrapper
+{
+    /**
+     * The original data.
+     *
+     * @var mixed
+     */
+    protected $original;
+
+    /**
+     * The wrapper constructor.
+     *
+     * @param  mixed  $original The original data.
+     * @return void
+     */
+    public function __construct($original)
+    {
+        $this->original = $original;
+    }
+
+    /**
+     * Gets the current original data.
+     *
+     * @return mixed
+     */
+    public function getOriginal()
+    {
+        return $this->original;
+    }
+}
