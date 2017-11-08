d4774489f799168eb0cb3aa5351e01d1b9aa79b0
Qingshan Luo <shanshan.lqs@gmail.com>
2017-11-08 17:59:16 +0800

    [TEST] Add unit test cases.

---
+++ Tests/Support/TestReturnExceptionBlocker.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Flow\Tests\Support;
+
+use Edoger\Container\Container;
+use Edoger\Flow\Contracts\Blocker;
+use Throwable;
+
+class TestReturnExceptionBlocker implements Blocker
+{
+    public function block(Container $input, Throwable $exception = null)
+    {
+        return $exception;
+    }
+}
