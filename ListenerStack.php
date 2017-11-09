2b31744661f048d43a7b14bb559e5711b61499b6
Qingshan Luo <shanshan.lqs@gmail.com>
2017-11-09 10:49:13 +0800

    [Event] Add event dispatcher.

---
+++ ListenerStack.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Event;
+
+use Edoger\Container\Stack;
+
+class ListenerStack extends Stack
+{
+    /**
+     * Marks whether the current event listener stack is enabled.
+     *
+     * @var boolean
+     */
+    protected $enabled = true;
+
+    /**
+     * Determines whether the current event listener stack is enabled.
+     *
+     * @return boolean
+     */
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Enable the current event listener stack.
+     *
+     * @return self
+     */
+    public function enable()
+    {
+        $this->enabled = true;
+
+        return $this;
+    }
+
+    /**
+     * Disable the current event listener stack.
+     *
+     * @return self
+     */
+    public function disable()
+    {
+        $this->enabled = false;
+
+        return $this;
+    }
+}
