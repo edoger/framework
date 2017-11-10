9943902824e363e7d1a9378340b782d46f525017
Qingshan Luo <shanshan.lqs@gmail.com>
2017-11-10 17:47:15 +0800

    [Logger] Add log handle flow blocker.

---
+++ src/Edoger/Logger/Blocker.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Logger;
+
+use Edoger\Container\Container;
+use Edoger\Flow\Contracts\Blocker as BlockerContract;
+use Throwable;
+
+class Blocker implements BlockerContract
+{
+    /**
+     * The log handle flow blocker constructor.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Block the current log handle stack.
+     *
+     * @param  Edoger\Container\Container $input     The processor input parameter container.
+     * @param  Throwable|null             $exception The captured processor exception.
+     * @return Edoger\Config\Repository
+     */
+    public function block(Container $input, Throwable $exception = null)
+    {
+        return false;
+    }
+}
