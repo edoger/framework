6fdcf34f0dd7503624eea5da7c03a6c5be756215
Qingshan Luo <shanshan.lqs@gmail.com>
2017-11-16 11:55:21 +0800

    [TEST] Add unit test cases for HTTP response cookie.

---
+++ src/Edoger/Http/Tests/Cases/Server/Response/CookieTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Http\Tests\Cases\Server\Response;
+
+use Edoger\Http\Server\Response\Cookie;
+use PHPUnit\Framework\TestCase;
+
+class CookieTest extends TestCase
+{
+    public function testCookieGetName()
+    {
+        $cookie = new Cookie('test');
+
+        $this->assertEquals('test', $cookie->getName());
+    }
+
+    public function testCookieGetValue()
+    {
+        $cookie = new Cookie('test');
+        $this->assertEquals('', $cookie->getValue());
+
+        $cookie = new Cookie('test', 'test');
+        $this->assertEquals('test', $cookie->getValue());
+    }
+
+    public function testCookieGetExpiresTime()
+    {
+        $cookie = new Cookie('test');
+        $this->assertEquals(0, $cookie->getExpiresTime());
+
+        $cookie = new Cookie('test', 'test', -86400);
+        $this->assertEquals(-86400, $cookie->getExpiresTime());
+
+        $cookie = new Cookie('test', 'test', 86400);
+        $this->assertEquals(86400, $cookie->getExpiresTime());
+    }
+
+    public function testCookieGetPath()
+    {
+        $cookie = new Cookie('test');
+        $this->assertEquals('/', $cookie->getPath());
+
+        $cookie = new Cookie('test', 'test', 0, '/test');
+        $this->assertEquals('/test', $cookie->getPath());
+    }
+
+    public function testCookieGetDomain()
+    {
+        $cookie = new Cookie('test');
+        $this->assertEquals('', $cookie->getDomain());
+
+        $cookie = new Cookie('test', 'test', 0, '/', '*.test.com');
+        $this->assertEquals('*.test.com', $cookie->getDomain());
+    }
+
+    public function testCookieIsSecure()
+    {
+        $cookie = new Cookie('test');
+        $this->assertFalse($cookie->isSecure());
+
+        $cookie = new Cookie('test', 'test', 0, '/', '', true);
+        $this->assertTrue($cookie->isSecure());
+    }
+
+    public function testCookieIsHttpOnly()
+    {
+        $cookie = new Cookie('test');
+        $this->assertFalse($cookie->isHttpOnly());
+
+        $cookie = new Cookie('test', 'test', 0, '/', '', false, true);
+        $this->assertTrue($cookie->isHttpOnly());
+    }
+}
---
+++ src/Edoger/Http/Tests/Cases/Server/Response/ExpiredCookieTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Http\Tests\Cases\Server\Response;
+
+use Edoger\Http\Server\Response\Cookie;
+use Edoger\Http\Server\Response\ExpiredCookie;
+use PHPUnit\Framework\TestCase;
+
+class ExpiredCookieTest extends TestCase
+{
+    public function testExpiredCookieExtendsCookie()
+    {
+        $cookie = new ExpiredCookie('test');
+
+        $this->assertInstanceOf(Cookie::class, $cookie);
+    }
+
+    public function testExpiredCookieConstructor()
+    {
+        $cookie = new ExpiredCookie('test');
+
+        $this->assertEquals('test', $cookie->getName());
+        $this->assertEquals('', $cookie->getValue());
+        $this->assertEquals(-31536000, $cookie->getExpiresTime());
+        $this->assertEquals('/', $cookie->getPath());
+        $this->assertEquals('', $cookie->getDomain());
+        $this->assertFalse($cookie->isSecure());
+        $this->assertFalse($cookie->isHttpOnly());
+
+        $cookie = new ExpiredCookie('test', '/test', '*.test.com', true, true);
+
+        $this->assertEquals('test', $cookie->getName());
+        $this->assertEquals('', $cookie->getValue());
+        $this->assertEquals(-31536000, $cookie->getExpiresTime());
+        $this->assertEquals('/test', $cookie->getPath());
+        $this->assertEquals('*.test.com', $cookie->getDomain());
+        $this->assertTrue($cookie->isSecure());
+        $this->assertTrue($cookie->isHttpOnly());
+    }
+}
