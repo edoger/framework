5c9cade9149aa8b52b8a0f77468ddd3b0017b90e
Qingshan Luo <shanshan.lqs@gmail.com>
2017-11-28 21:29:07 +0800

    Edoger Framework Version.

---
+++ src/Edoger/Version.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger;
+
+class Version
+{
+    /**
+     * The version of the current Edoger framework.
+     */
+    const VERSION = '1.0.0-dev';
+
+    /**
+     * Get the version of the current Edoger framework.
+     *
+     * @return string
+     */
+    public static function getVersion(): string
+    {
+        return self::VERSION;
+    }
+}
