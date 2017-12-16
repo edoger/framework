04aa1a1cc402488534462a24bf45d1731099d097
Qingshan Luo <shanshan.lqs@gmail.com>
2017-12-17 00:52:05 +0800

    [TEST] Add unit test cases for native session.

---
+++ src/Edoger/Session/Tests/Cases/NativeSessionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Session\Tests\Cases;
+
+use PHPUnit\Framework\TestCase;
+use Edoger\Session\NativeSession;
+use Edoger\Session\AbstractSession;
+use Edoger\Session\Stores\GlobalSessionStore;
+use Edoger\Session\Tests\Support\TestSessionHandler;
+
+class NativeSessionTest extends TestCase
+{
+    public function testNativeSessionExtendsAbstractSession()
+    {
+        $session = new NativeSession(new TestSessionHandler());
+
+        $this->assertInstanceOf(AbstractSession::class, $session);
+    }
+
+    public function testNativeSessionUsedGlobalSessionStore()
+    {
+        $store = (new NativeSession(new TestSessionHandler()))->getSessionStore();
+
+        $this->assertInstanceOf(GlobalSessionStore::class, $store);
+    }
+}
