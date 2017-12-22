88763b2a7fb431b326f0279b355a167db5f9c053
Qingshan Luo <shanshan.lqs@gmail.com>
2017-12-22 23:33:56 +0800

    [Util] Add Edoger\Util\Validator class.

---
+++ src/Edoger/Util/Tests/Cases/ValidatorTest.php
@@ -0,0 +1,219 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Util\Tests\Cases;
+
+use Edoger\Util\Validator;
+use PHPUnit\Framework\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    public function testValidatorIsNotEmptyString()
+    {
+        $this->assertFalse(Validator::isNotEmptyString(''));
+        $this->assertFalse(Validator::isNotEmptyString(null));
+        $this->assertFalse(Validator::isNotEmptyString(1));
+        $this->assertFalse(Validator::isNotEmptyString([]));
+        $this->assertTrue(Validator::isNotEmptyString('0'));
+        $this->assertTrue(Validator::isNotEmptyString('foo'));
+    }
+
+    public function testValidatorIsNumber()
+    {
+        $this->assertFalse(Validator::isNumber(''));
+        $this->assertFalse(Validator::isNumber(null));
+        $this->assertFalse(Validator::isNumber([]));
+        $this->assertFalse(Validator::isNumber('0'));
+        $this->assertFalse(Validator::isNumber('foo'));
+        $this->assertTrue(Validator::isNumber(1));
+        $this->assertTrue(Validator::isNumber(1.5));
+        $this->assertTrue(Validator::isNumber(0));
+        $this->assertTrue(Validator::isNumber(-1));
+        $this->assertTrue(Validator::isNumber(-1.5));
+    }
+
+    public function testValidatorIsStringOrNumber()
+    {
+        $this->assertFalse(Validator::isStringOrNumber(null));
+        $this->assertFalse(Validator::isStringOrNumber([]));
+        $this->assertTrue(Validator::isStringOrNumber('0'));
+        $this->assertTrue(Validator::isStringOrNumber('foo'));
+        $this->assertTrue(Validator::isStringOrNumber(1));
+        $this->assertTrue(Validator::isStringOrNumber(1.5));
+        $this->assertTrue(Validator::isStringOrNumber(0));
+        $this->assertTrue(Validator::isStringOrNumber(-1));
+        $this->assertTrue(Validator::isStringOrNumber(-1.5));
+        $this->assertTrue(Validator::isStringOrNumber('1'));
+        $this->assertTrue(Validator::isStringOrNumber('1.5'));
+        $this->assertTrue(Validator::isStringOrNumber(0));
+        $this->assertTrue(Validator::isStringOrNumber('-1'));
+        $this->assertTrue(Validator::isStringOrNumber('-1.5'));
+        $this->assertTrue(Validator::isStringOrNumber(0xf));
+    }
+
+    public function testValidatorIsPositiveNumber()
+    {
+        $this->assertFalse(Validator::isPositiveNumber(''));
+        $this->assertFalse(Validator::isPositiveNumber(null));
+        $this->assertFalse(Validator::isPositiveNumber([]));
+        $this->assertFalse(Validator::isPositiveNumber('0'));
+        $this->assertFalse(Validator::isPositiveNumber('foo'));
+        $this->assertTrue(Validator::isPositiveNumber(1));
+        $this->assertTrue(Validator::isPositiveNumber(1.5));
+        $this->assertFalse(Validator::isPositiveNumber(0));
+        $this->assertFalse(Validator::isPositiveNumber(-1));
+        $this->assertFalse(Validator::isPositiveNumber(-1.5));
+    }
+
+    public function testValidatorIsPositiveInteger()
+    {
+        $this->assertFalse(Validator::isPositiveInteger(''));
+        $this->assertFalse(Validator::isPositiveInteger(null));
+        $this->assertFalse(Validator::isPositiveInteger([]));
+        $this->assertFalse(Validator::isPositiveInteger('0'));
+        $this->assertFalse(Validator::isPositiveInteger('foo'));
+        $this->assertTrue(Validator::isPositiveInteger(1));
+        $this->assertFalse(Validator::isPositiveInteger(1.5));
+        $this->assertFalse(Validator::isPositiveInteger(0));
+        $this->assertFalse(Validator::isPositiveInteger(-1));
+        $this->assertFalse(Validator::isPositiveInteger(-1.5));
+    }
+
+    public function testValidatorIsNegativeNumber()
+    {
+        $this->assertFalse(Validator::isNegativeNumber(''));
+        $this->assertFalse(Validator::isNegativeNumber(null));
+        $this->assertFalse(Validator::isNegativeNumber([]));
+        $this->assertFalse(Validator::isNegativeNumber('0'));
+        $this->assertFalse(Validator::isNegativeNumber('foo'));
+        $this->assertFalse(Validator::isNegativeNumber(1));
+        $this->assertFalse(Validator::isNegativeNumber(1.5));
+        $this->assertFalse(Validator::isNegativeNumber(0));
+        $this->assertTrue(Validator::isNegativeNumber(-1));
+        $this->assertTrue(Validator::isNegativeNumber(-1.5));
+    }
+
+    public function testValidatorIsNegativeInteger()
+    {
+        $this->assertFalse(Validator::isNegativeInteger(''));
+        $this->assertFalse(Validator::isNegativeInteger(null));
+        $this->assertFalse(Validator::isNegativeInteger([]));
+        $this->assertFalse(Validator::isNegativeInteger('0'));
+        $this->assertFalse(Validator::isNegativeInteger('foo'));
+        $this->assertFalse(Validator::isNegativeInteger(1));
+        $this->assertFalse(Validator::isNegativeInteger(1.5));
+        $this->assertFalse(Validator::isNegativeInteger(0));
+        $this->assertTrue(Validator::isNegativeInteger(-1));
+        $this->assertFalse(Validator::isNegativeInteger(-1.5));
+    }
+
+    public function testValidatorIsIpv4()
+    {
+        $this->assertFalse(Validator::isIpv4(''));
+        $this->assertFalse(Validator::isIpv4('123'));
+        $this->assertFalse(Validator::isIpv4(123));
+        $this->assertTrue(Validator::isIpv4('123.123.123.123'));
+        $this->assertTrue(Validator::isIpv4('0.0.0.0'));
+        $this->assertTrue(Validator::isIpv4('255.255.255.255'));
+        $this->assertTrue(Validator::isIpv4('192.168.12.12'));
+        $this->assertTrue(Validator::isIpv4('127.0.0.1'));
+
+        $this->assertFalse(Validator::isIpv4('192.168.12.12', true));
+        $this->assertFalse(Validator::isIpv4('127.0.0.1', false, true));
+    }
+
+    public function testValidatorIsIpv6()
+    {
+        $this->assertFalse(Validator::isIpv6(''));
+        $this->assertFalse(Validator::isIpv6('123'));
+        $this->assertFalse(Validator::isIpv6(123));
+        $this->assertTrue(Validator::isIpv6('ABCD:EF01:2345:6789:ABCD:EF01:2345:6789'));
+        $this->assertTrue(Validator::isIpv6('::'));
+        $this->assertTrue(Validator::isIpv6('FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF'));
+        $this->assertTrue(Validator::isIpv6('FD:FD:FD:FD:FD:FD:FD:FD'));
+        $this->assertTrue(Validator::isIpv6('FC:FC:FC:FC:FC:FC:FC:FC'));
+
+        $this->assertFalse(Validator::isIpv6('FD:FD:FD:FD:FD:FD:FD:FD', true));
+        $this->assertFalse(Validator::isIpv6('FD::FD', true));
+        $this->assertFalse(Validator::isIpv6('FC::FC', true));
+        $this->assertFalse(Validator::isIpv6('::1', false, true));
+        $this->assertFalse(Validator::isIpv6('::FFFF::', false, true));
+        $this->assertFalse(Validator::isIpv6('FE80::', false, true));
+    }
+
+    public function testValidatorIsEmail()
+    {
+        $this->assertFalse(Validator::isEmail(''));
+        $this->assertFalse(Validator::isEmail('@foo.com'));
+        $this->assertFalse(Validator::isEmail('foo@'));
+        $this->assertFalse(Validator::isEmail('foo@foo'));
+        $this->assertFalse(Validator::isEmail('foo@foo..com'));
+        $this->assertFalse(Validator::isEmail('f..oo@foo.com'));
+        $this->assertTrue(Validator::isEmail('foo@foo.com'));
+        $this->assertTrue(Validator::isEmail('f-oo@foo.com'));
+        $this->assertTrue(Validator::isEmail('f.oo@foo.com'));
+        $this->assertTrue(Validator::isEmail('f.o.o@foo.com'));
+    }
+
+    public function testValidatorIsIntergerString()
+    {
+        $this->assertFalse(Validator::isIntergerString(''));
+        $this->assertFalse(Validator::isIntergerString('foo'));
+        $this->assertFalse(Validator::isIntergerString('0123'));
+        $this->assertFalse(Validator::isIntergerString(123));
+        $this->assertTrue(Validator::isIntergerString('123'));
+    }
+
+    public function testValidatorIsMobileNumber()
+    {
+        $this->assertFalse(Validator::isMobileNumber(''));
+        $this->assertFalse(Validator::isMobileNumber('12345'));
+        $this->assertFalse(Validator::isMobileNumber('01212341234'));
+        $this->assertTrue(Validator::isMobileNumber('13312341234'));
+        $this->assertTrue(Validator::isMobileNumber('12312341234'));
+    }
+
+    public function testValidatorIsTelNumber()
+    {
+        $this->assertFalse(Validator::isTelNumber(''));
+        $this->assertFalse(Validator::isTelNumber('123--'));
+        $this->assertFalse(Validator::isTelNumber('123-1234-'));
+        $this->assertTrue(Validator::isTelNumber('12345'));
+        $this->assertTrue(Validator::isTelNumber('01212341234'));
+        $this->assertTrue(Validator::isTelNumber('13312341234'));
+        $this->assertTrue(Validator::isTelNumber('12312341234'));
+        $this->assertTrue(Validator::isTelNumber('123-12341234'));
+        $this->assertTrue(Validator::isTelNumber('123-1234-1234'));
+        $this->assertTrue(Validator::isTelNumber('0123-1234-1234'));
+    }
+
+    public function testValidatorIsDomainName()
+    {
+        $this->assertFalse(Validator::isDomainName(''));
+        $this->assertFalse(Validator::isDomainName('foo'));
+        $this->assertFalse(Validator::isDomainName('_.com'));
+        $this->assertTrue(Validator::isDomainName('foo.com'));
+        $this->assertTrue(Validator::isDomainName('foo.bar.com'));
+        $this->assertTrue(Validator::isDomainName('foo.bar.baz.com'));
+        $this->assertTrue(Validator::isDomainName('1.123.com'));
+    }
+
+    public function testValidatorIsAttributeName()
+    {
+        $this->assertFalse(Validator::isAttributeName(''));
+        $this->assertFalse(Validator::isAttributeName('a:b:c'));
+        $this->assertFalse(Validator::isAttributeName('a.b'));
+        $this->assertFalse(Validator::isAttributeName('1ab'));
+        $this->assertTrue(Validator::isAttributeName('abc'));
+        $this->assertTrue(Validator::isAttributeName('_abc'));
+        $this->assertTrue(Validator::isAttributeName('abc1'));
+        $this->assertTrue(Validator::isAttributeName('abc_5'));
+    }
+}
---
+++ src/Edoger/Util/Validator.php
@@ -0,0 +1,251 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Util;
+
+class Validator
+{
+    /**
+     * Determines if the given value is a non-empty string.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isNotEmptyString($value): bool
+    {
+        return is_string($value) && '' !== $value;
+    }
+
+    /**
+     * Determines if the given value is a numeric value.
+     * Notice: Any string will not be considered a numeric value.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isNumber($value): bool
+    {
+        return is_int($value) || is_float($value);
+    }
+
+    /**
+     * Determines if the given value is a number or a string.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isStringOrNumber($value): bool
+    {
+        return is_string($value) || is_numeric($value);
+    }
+
+    /**
+     * Determines if the given value is a positive number.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isPositiveNumber($value): bool
+    {
+        return static::isNumber($value) && $value > 0;
+    }
+
+    /**
+     * Determines if the given value is a positive integer.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isPositiveInteger($value): bool
+    {
+        return is_int($value) && $value > 0;
+    }
+
+    /**
+     * Determines if the given value is a negative number.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isNegativeNumber($value): bool
+    {
+        return static::isNumber($value) && $value < 0;
+    }
+
+    /**
+     * Determines if the given value is a negative integer.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isNegativeInteger($value): bool
+    {
+        return is_int($value) && $value < 0;
+    }
+
+    /**
+     * Determine if the given value is an IPv4 address.
+     *
+     * @param mixed $value    The given value.
+     * @param bool  $private  Whether or not to allow for private address.
+     * @param bool  $reserved Whether or not to allow for the reserved address.
+     *
+     * @return bool
+     */
+    public static function isIpv4($value, bool $private = false, bool $reserved = false): bool
+    {
+        if (!static::isNotEmptyString($value)) {
+            return false;
+        }
+
+        $flags = FILTER_FLAG_IPV4;
+
+        if ($private) {
+            $flags |= FILTER_FLAG_NO_PRIV_RANGE;
+        }
+
+        if ($reserved) {
+            $flags |= FILTER_FLAG_NO_RES_RANGE;
+        }
+
+        return false !== filter_var($value, FILTER_VALIDATE_IP, $flags);
+    }
+
+    /**
+     * Determine if the given value is an IPv6 address.
+     *
+     * @param mixed $value    The given value.
+     * @param bool  $private  Whether or not to allow for private address.
+     * @param bool  $reserved Whether or not to allow for the reserved address.
+     *
+     * @return bool
+     */
+    public static function isIpv6($value, bool $private = false, bool $reserved = false): bool
+    {
+        if (!static::isNotEmptyString($value)) {
+            return false;
+        }
+
+        $flags = FILTER_FLAG_IPV6;
+
+        if ($private) {
+            $flags |= FILTER_FLAG_NO_PRIV_RANGE;
+        }
+
+        if ($reserved) {
+            $flags |= FILTER_FLAG_NO_RES_RANGE;
+        }
+
+        return false !== filter_var($value, FILTER_VALIDATE_IP, $flags);
+    }
+
+    /**
+     * Determine if the given value is an email address.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isEmail($value): bool
+    {
+        if (!static::isNotEmptyString($value)) {
+            return false;
+        }
+
+        return false !== filter_var($value, FILTER_VALIDATE_EMAIL, FILTER_FLAG_EMAIL_UNICODE);
+    }
+
+    /**
+     * Determines if the given value is an integer string.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isIntergerString($value): bool
+    {
+        if (!static::isNotEmptyString($value)) {
+            return false;
+        }
+
+        return (bool) preg_match('/^[1-9]\d*$/', $value);
+    }
+
+    /**
+     * Determine if the given value is a mobile phone number.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isMobileNumber($value): bool
+    {
+        if (!static::isNotEmptyString($value)) {
+            return false;
+        }
+
+        return (bool) preg_match('/^1\d{10}$/', $value);
+    }
+
+    /**
+     * Determine if the given value is a tel number.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isTelNumber($value): bool
+    {
+        if (!static::isNotEmptyString($value)) {
+            return false;
+        }
+
+        return (bool) preg_match('/^\d+(?:\-\d+)*$/', $value);
+    }
+
+    /**
+     * Determine if the given value is a domain name.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isDomainName($value): bool
+    {
+        if (!static::isNotEmptyString($value)) {
+            return false;
+        }
+
+        return (bool) preg_match('/^[a-z\d\-]+(?:\.[a-z\d\-]+)+$/', $value);
+    }
+
+    /**
+     * Determine whether a given value is an attribute name.
+     *
+     * @param mixed $value The given value.
+     *
+     * @return bool
+     */
+    public static function isAttributeName($value): bool
+    {
+        if (!static::isNotEmptyString($value)) {
+            return false;
+        }
+
+        return (bool) preg_match('/^[a-zA-Z_]\w*$/', $value);
+    }
+}
