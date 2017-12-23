4728cbb40ad58cbcf68eb06d3f4c333898c95ef1
Qingshan Luo <shanshan.lqs@gmail.com>
2017-12-23 23:12:57 +0800

    [Util] Add Edoger\Util\Environment class.

---
+++ src/Edoger/Util/Environment.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Util;
+
+class Environment
+{
+    /**
+     * Determine whether the current runtime environment is windows.
+     *
+     * @return bool
+     */
+    public static function isWindows(): bool
+    {
+        return '\\' === DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * Determine if the current runtime environment is CLI.
+     *
+     * @return bool
+     */
+    public static function isCli(): bool
+    {
+        return 'cli' === PHP_SAPI;
+    }
+}
