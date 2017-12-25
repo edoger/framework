d3a73653e277efd89928b9cbb0f27282c885c209
Qingshan Luo <shanshan.lqs@gmail.com>
2017-12-25 14:50:21 +0800

    [Http] Add HTTP simple response.

---
+++ src/Edoger/Http/Server/Response/SimpleResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Http\Server\Response;
+
+use Edoger\Http\Server\Response\Renderers\SimpleRenderer;
+
+class SimpleResponse extends Response
+{
+    /**
+     * The simple response constructor.
+     *
+     * @param int      $status  The HTTP response status code.
+     * @param string   $body    The response body.
+     * @param iterable $headers The response headers.
+     *
+     * @return void
+     */
+    public function __construct(int $status, string $body, iterable $headers = [])
+    {
+        parent::__construct($status, [], $headers);
+
+        // Response body content is given by the constructor.
+        // Response data collection will no longer affect the response body.
+        $this->setResponseRenderer(SimpleRenderer::create($body));
+    }
+}
