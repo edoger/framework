b431f800529ff133dfce9d8447acc1903be8ecdf
Qingshan Luo <shanshan.lqs@gmail.com>
2018-01-09 21:44:26 +0800

    [Database] Add database exception class.

---
+++ src/Edoger/Database/Exceptions/DatabaseException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 - 2018 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Database\Exceptions;
+
+use Exception;
+
+class DatabaseException extends Exception
+{
+    // The database exception class.
+}
