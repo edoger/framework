d1c819d23a3a8828a04e65b693cf407e7af9e5d4
Qingshan Luo <shanshan.lqs@gmail.com>
2018-01-09 23:37:39 +0800

    [Database] Add MySQL unix socket server.

---
+++ src/Edoger/Database/MySQL/SocketServer.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 - 2018 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Database\MySQL;
+
+use InvalidArgumentException;
+
+class SocketServer extends Server
+{
+    /**
+     * The unix socket server constructor.
+     *
+     * @param string $name     The current server name.
+     * @param string $socket   The current server unix socket path.
+     * @param string $username The current server user name.
+     * @param string $password The current server user password.
+     * @param string $dbname   The current server default database name.
+     * @param string $charset  The current server default client charset.
+     *
+     * @throws InvalidArgumentException Thrown when the MySQL unix socket path is invalid.
+     *
+     * @return void
+     */
+    public function __construct(string $name, string $socket, string $username = 'root', string $password = '', string $dbname = '', string $charset = 'utf8mb4')
+    {
+        if ('' === $socket) {
+            throw new InvalidArgumentException('Invalid MySQL unix socket path.');
+        }
+
+        parent::__construct($name, $username, $password, '', 0, $socket, $dbname, $charset);
+    }
+}
