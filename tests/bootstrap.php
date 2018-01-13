da7ddf5721abb3bd865feb1dcf885524be63a213
Qingshan Luo <shanshan.lqs@gmail.com>
2018-01-13 20:48:29 +0800

    [TEST] Define test MySQL database account.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,3 +14,16 @@ define('TEST_ENV', $_ENV['TEST_ENV'] ?? $_SERVER['TEST_ENV'] ?? 'LOCAL');
 
 // Enable Composer autoloader.
 require __DIR__.'/../vendor/autoload.php';
+
+// Define test MySQL database account.
+if ('TRAVIS_CI' === TEST_ENV) {
+    define('TEST_MYSQL_USERNAME', 'edoger');
+    define('TEST_MYSQL_PASSWORD', 'edoger');
+} elseif ('LOCAL' === TEST_ENV) {
+    define('TEST_MYSQL_USERNAME', $_ENV['TEST_MYSQL_USERNAME'] ?? $_SERVER['TEST_MYSQL_USERNAME'] ?? 'edoger');
+    define('TEST_MYSQL_PASSWORD', $_ENV['TEST_MYSQL_PASSWORD'] ?? $_SERVER['TEST_MYSQL_PASSWORD'] ?? 'edoger');
+} else {
+    echo 'Invalid test environment.'.PHP_EOL;
+    // End the test, but this is not considered a test failure.
+    exit(0);
+}
