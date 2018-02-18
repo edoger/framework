1ef3e1e844afb118c238f6b2e87faa5e5cc2ed76
Qingshan Luo <shanshan.lqs@gmail.com>
2018-02-18 14:31:17 +0800

    [Util] Add interface Edoger\Util\Contracts\Wrapper.

---
+++ src/Edoger/Util/Contracts/Wrapper.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 - 2018 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace Edoger\Util\Contracts;
+
+interface Wrapper
+{
+    /**
+     * Gets the original value.
+     *
+     * @return mixed
+     */
+    public function getOriginal();
+}
