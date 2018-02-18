90e9e1e386657a9cba9a88daf2af5c8a960ed2fd
Qingshan Luo <shanshan.lqs@gmail.com>
2018-02-18 14:39:50 +0800

    [TEST] Add test cases for Edoger\Util\Util class.

---
+++ tests/EdogerTests/Util/Cases/UtilTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 - 2018 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace EdogerTests\Util\Cases;
+
+use Edoger\Util\Util;
+use PHPUnit\Framework\TestCase;
+use EdogerTests\Util\Mocks\TestWrapper;
+
+class UtilTest extends TestCase
+{
+    public function testUtilValue()
+    {
+        $this->assertEquals('foo', Util::value('foo'));
+        $this->assertEquals('foo', Util::value(new TestWrapper('foo')));
+        $this->assertEquals('foo', Util::value(function () {
+            return 'foo';
+        }));
+    }
+}
---
+++ tests/EdogerTests/Util/Mocks/TestWrapper.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Edoger framework.
+ *
+ * @author    Qingshan Luo <shanshan.lqs@gmail.com>
+ * @copyright 2017 - 2018 Qingshan Luo
+ * @license   GNU Lesser General Public License 3.0
+ */
+
+namespace EdogerTests\Util\Mocks;
+
+use Edoger\Util\Contracts\Wrapper;
+
+class TestWrapper implements Wrapper
+{
+    protected $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function getOriginal()
+    {
+        return $this->value;
+    }
+}
