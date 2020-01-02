2dba923dd10af10dc1a7eaebc5625a4343f3d2c9
Qingshan Luo <shanshan.lqs@gmail.com>
2020-01-02 10:48:40 +0800

    Year 2020

--- src/Edoger/Http/Server/Response/Renderers/EmptyRenderer.php
+++ src/Edoger/Http/Server/Response/Renderers/EmptyRenderer.php
@@ -4,7 +4,7 @@
  * This file is part of the Edoger framework.
  *
  * @author    Qingshan Luo <shanshan.lqs@gmail.com>
- * @copyright 2017 - 2019 Qingshan Luo
+ * @copyright 2017 - 2020 Qingshan Luo
  * @license   GNU Lesser General Public License 3.0
  */
 
